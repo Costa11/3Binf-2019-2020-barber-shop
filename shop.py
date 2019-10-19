463896515057d28a22ecd6736d379e574327e6a6
Your Name <you@example.com>
2019-10-19 07:49:04 +0200

    fix tests

--- shop.py
+++ shop.py
@@ -1,3 +1,13 @@
 class Shop:
-    def __init__(self,address,a,b,c,d,e,f):
+    def __init__(self,address,open_hour,close_hour,min_price,max_price,avg_price,f):
         self.address = address
+        self.open_hour = open_hour
+        self.close_hour = close_hour
+        self.min_price = min_price
+        self.max_price = max_price
+        self.avg_price = (max_price + min_price) / 2
+    def is_open(self, now):
+        if now > self.open_hour and now < self.close_hour :
+            return True
+        else:
+            return False
